2afda170b68fce4c5014b276e8d0fd2178abd17b
Sangyao42 <wangsangyao@hotmail.com>
2022-11-13 22:55:01 +0100

    Sorted get one line logic
    Added utils functions

--- src/get_next_line.c
+++ src/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: sawang <sawang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 20:45:39 by sawang            #+#    #+#             */
-/*   Updated: 2022/11/13 14:35:26 by sawang           ###   ########.fr       */
+/*   Updated: 2022/11/13 22:50:47 by sawang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,27 +22,39 @@ char	*get_next_line(int fd)
 	char		*line;
 	static char	*offset_str;
 
-	line = get_one_line(offset_str);
+	line = get_one_line(fd, offset_str);
 	return (line);
 }
 
 /**
- * @var buffer
 */
-char	*get_one_line(char *offset_str)
+char	*get_one_line(int fd, char *offset_str)
 {
 	char	*first_line;
-	char	*buffer;
+	int		offset;
+	// char	*buffer;
 	// buffer for all buffer red by read????
 	// char	*file_already_red;
 
 	//?	file_already_red = malloc();
-	buffer = read_file(fd, BUFFER_SIZE);
-	int	 count= ft_strchr(offset_str,'\n');
-	first_line = ft_substr(offset_str, 0, count);
-	*offset_str = *offset_str + count;
-
-	return (first_line);
+	*offset_str = "";
+	//offset_str = read_file(fd, BUFFER_SIZE);
+	while (offset_str)
+	{
+		offset_str = ft_strjoin(offset_str, read_file(fd, BUFFER_SIZE));
+		offset = ft_strchr(offset_str, '\n');
+		if (offset) //if \n exists in offset_str
+		{
+			first_line = ft_substr(offset_str, 0, offset);
+			return (first_line);
+		}
+		else//???
+			first_line = offset_str;
+			//?
+		offset_str = offset_str + ft_strlen(first_line);
+	}
+	// return (first_line);
+	return
 }
 
 char	*read_file(int fd, int count)
--- src/get_next_line_utils.c
+++ src/get_next_line_utils.c
@@ -6,8 +6,82 @@
 /*   By: sawang <sawang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 16:03:38 by sawang            #+#    #+#             */
-/*   Updated: 2022/11/12 19:09:29 by sawang           ###   ########.fr       */
+/*   Updated: 2022/11/13 22:54:27 by sawang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	i;
+	char	*str_join;
+
+	if (!s1 || !s2)
+		return (NULL);
+	str_join = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!str_join)
+		return (NULL);
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		str_join[i] = s1[i];
+		i++;
+	}
+	i = 0;
+	while (s2[i] != '\0')
+	{
+		str_join[ft_strlen(s1) + i] = s2[i];
+		i++;
+	}
+	str_join[ft_strlen(s1) + i] = '\0';
+	return (str_join);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	size_t	i;
+	size_t	s_len;
+	char	*str;
+
+	str = (char *)s;
+	s_len = ft_strlen(str);
+	if ((char) c == 0)
+		return (&str[s_len]);
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == (char) c)
+			return (&str[i]);
+		i++;
+	}
+	return (NULL);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*substr;
+	size_t	i;
+	size_t	s_len;
+
+	if (!s)
+		return (NULL);
+	s_len = ft_strlen(s);
+	if ((size_t)start >= s_len)
+		substr = (char *)malloc(1);
+	else if (len > s_len)
+		substr = (char *)malloc(s_len - (size_t)start + 1);
+	else
+		substr = (char *)malloc(len + 1);
+	if (!substr)
+		return (NULL);
+	i = 0;
+	while (i < len && (size_t)start < s_len)
+	{
+		substr[i] = s[start];
+		i++;
+		start++;
+	}
+	substr[i] = '\0';
+	return (substr);
+}
